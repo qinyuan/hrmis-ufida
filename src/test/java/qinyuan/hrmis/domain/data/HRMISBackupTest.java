f0e0df67fbe187ae6f304c07f0c43e3ddcb4222c
qinyuan <yuanq@rayootech.com>
2013-12-29 22:48:51 +0800

    add test case files

--- src/test/java/qinyuan/hrmis/domain/data/HRMISBackupTest.java
+++ src/test/java/qinyuan/hrmis/domain/data/HRMISBackupTest.java
@@ -6,20 +6,20 @@ import java.io.File;
 
 import org.junit.Test;
 
+import qinyuan.hrmis.HrmisConfig;
 import qinyuan.hrmis.domain.data.HRMISBackup;
 
 public class HRMISBackupTest {
 
 	@Test
 	public void test() throws Exception {
-		HRMISBackup backup=new HRMISBackup();
-		assertTrue(backup.export("e:/"));
-	
-		String fileName= backup.getFileName();
-		File file=new File(fileName);
-		
+		HRMISBackup backup = new HRMISBackup();
+		String fileName = backup.export(HrmisConfig.getDataFolderName());
+
+		File file = new File(fileName);
+
 		assertTrue(file.exists());
-		
+
 		file.delete();
 		assertFalse(file.exists());
 	}
