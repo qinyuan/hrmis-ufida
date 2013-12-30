596938feda18a60fc0146a76a89a9e6760a34ad9
qinyuan <yuanq@rayootech.com>
2013-12-30 10:04:03 +0800

    create debugSql.jsp

--- src/main/java/qinyuan/hrmis/servlet/Backup.java
+++ src/main/java/qinyuan/hrmis/servlet/Backup.java
@@ -18,7 +18,10 @@ public class Backup extends MyServlet {
 		} catch (Exception e) {
 			e.printStackTrace();
 			print("数据库操作失败");
-			print("<br />"+e.getMessage());
+			print("<br />" + e.getMessage());
+			for (StackTraceElement sts : e.getStackTrace()) {
+				print("<br />" + sts.toString());
+			}
 		}
 	}
 }
