7656947aa7d5c6aaa9692cb34f9cd6c46e2442e8
qinyuan <qinyuan15@sina.com>
2015-08-13 03:01:09 +0800

    delete data_source.properties

--- src/test/java/qinyuan/hrmis/domain/user/UserDaoTest.java
+++ src/test/java/qinyuan/hrmis/domain/user/UserDaoTest.java
@@ -4,7 +4,7 @@ import junit.framework.TestCase;
 
 public class UserDaoTest extends TestCase {
     public void testGetUser() throws Exception {
-        User user = UserDao.getUser("李四", "test");
+        User user = UserDao.getUser("覃源", "test");
         System.out.println(user);
         if (user != null) {
             System.out.println(user.getName());
